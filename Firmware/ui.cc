16c5d67786ef7f25ee3212e0a07e600952bbe74a
Josh Bassett <josh.bassett@gmail.com>
2016-07-27 07:58:25 +1000

    WIP

--- Firmware/ui.cc
+++ Firmware/ui.cc
@@ -2,9 +2,38 @@
 
 namespace penrose {
   void Ui::init() {
+    col_1.set_mode(DIGITAL_OUTPUT);
+    col_2.set_mode(DIGITAL_OUTPUT);
+    col_3.set_mode(DIGITAL_OUTPUT);
+    col_4.set_mode(DIGITAL_OUTPUT);
+
+    row_1.set_mode(DIGITAL_INPUT);
+    row_2.set_mode(DIGITAL_INPUT);
+    row_1.High();
+    row_2.High();
   }
 
-  void Ui::setLedState(uint8_t n, LedState ledState) {
-    ledStates[n] = ledState;
+  void Ui::poll() {
+    pollButton(currentButton++);
+
+    if (currentButton >= kNumButtons) {
+      currentButton = 0;
+    }
+  }
+
+  void Ui::pollButton(uint8_t n) {
+    // uint8_t row = n / 4;
+    // uint8_t col = n % 4;
+
+    col_1.High();
+    col_2.High();
+    col_3.High();
+    col_4.High();
+
+    row_1.value();
+  }
+
+  void Ui::setLedState(uint8_t n, LedState state) {
+    ledStates[n] = state;
   }
 }
--- Firmware/ui.h
+++ Firmware/ui.h
@@ -1,21 +1,60 @@
 #ifndef PENROSE_UI_H_
 #define PENROSE_UI_H_
 
-#include <avr/io.h>
+#include "avrlib/gpio.h"
 
-enum LedState {off, red, green};
+#define COL1_PIN PD0
+#define COL2_PIN PD1
+#define COL3_PIN PD2
+#define COL4_PIN PD3
+
+#define COL_PORT PORTD
+#define COL_DDR  DDRD
+
+#define SWITCH_ROW1_PIN	PB2
+#define SWITCH_ROW2_PIN	PB4
+#define SWITCH_ROW3_PIN	PC5
+
+#define SWITCH_INPUT_12	PINB
+#define SWITCH_DDR_12		DDRB
+#define SWITCH_PORT_12	PORTB
+#define SWITCH_INPUT_3	PINC
+#define SWITCH_DDR_3		DDRC
+#define SWITCH_PORT_3		PORTC
+
+using namespace avrlib;
 
 namespace penrose {
+  enum LedState {LED_STATE_OFF, LED_STATE_RED, LED_STATE_GREEN};
+  enum ButtonState {BUTTON_STATE_OFF, BUTTON_STATE_ON};
+
+  const uint8_t kNumButtons = 12;
+
   class Ui {
   public:
     Ui() {}
     ~Ui() {}
 
     void init();
-    void setLedState(uint8_t n, LedState ledState);
+    void poll();
+
+    void pollButton(uint8_t n);
+    void setLedState(uint8_t n, LedState state);
 
   private:
-    LedState ledStates[10];
+    uint8_t currentButton;
+
+    bool buttonStates[kNumButtons];
+    LedState ledStates[kNumButtons];
+
+    Gpio<PortD, 0> col_1;
+    Gpio<PortD, 1> col_2;
+    Gpio<PortD, 2> col_3;
+    Gpio<PortD, 3> col_4;
+
+    Gpio<PortB, 2> row_1;
+    Gpio<PortB, 4> row_2;
+    Gpio<PortC, 5> row_3;
   };
 }
 
